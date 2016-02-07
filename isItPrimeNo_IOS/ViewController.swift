1376e6b6cd3e7257360c764cac2ae4fc3c387bc0
venkat <vikkii89@gmail.com>
2016-02-06 22:45:58 -0800

    small app to findout if a number is prime or not

--- isItPrimeNo_IOS/ViewController.swift
+++ isItPrimeNo_IOS/ViewController.swift
@@ -20,6 +20,75 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var resultText: UILabel!
+    @IBOutlet weak var inputNo: UITextField!
 
+    
+    @IBAction func isPrime(sender: AnyObject) {
+        
+        let inp = Int(inputNo.text!)
+        var isPrime = true
+        if(inp != nil) {
+            let inputVal = inp!
+            var i = 2
+            if inp <= 1{
+                isPrime = false
+            }
+            
+            if inputVal != 2 && inputVal != 1 {
+                while i <= inputVal / 2  {
+                    
+                    if( inputVal % i == 0 && i != inputVal) {
+                        isPrime = false
+                    }
+                    i++
+                    
+                }
+                
+            }
+            if isPrime {
+                resultText.text = "Yes, it is!"
+            }else{
+                resultText.text = "No, it is not"
+            }
+
+        }else {
+            resultText.text = "Please enter a number"
+        }
+        
+    }
+//    @IBAction func isPrime(sender: AnyObject) {
+//        
+//        let inp:Int! = Int(inputNo.text!)
+//        var isPrime = true
+//        if(inp != nil) {
+//
+//            var i = 2
+//            if inp == 1 {
+//                isPrime = false
+//            }
+//            
+//            if inp != 2 && inp != 1 {
+//                while i <= inp / 2  {
+//                    
+//                    if( inp % i == 0 && i != inp) {
+//                        isPrime = false
+//                    }
+//                    i++
+//                    
+//                }
+//                
+//            }
+//            if isPrime {
+//                resultText.text = "Yes, it is!"
+//            }else{
+//                resultText.text = "No, it is not"
+//            }
+//            
+//        }else {
+//            resultText.text = "Please enter a number"
+//        }
+//        
+//    }
 }
 
